a337e0e22608d71d3fc7d0c1bc0e6df034d069e1
Zlaam <mrzalaam@gmail.com>
2024-07-01 17:52:45 +0500

    "Added state and effect to fetch data from API, updated component to render fetched data in a FlatList"

--- App.tsx
+++ App.tsx
@@ -1,13 +1,49 @@
-import {} from "react";
-import { View, Text } from "react-native";
-
+import { useEffect, useState } from "react";
+import { View, Text, FlatList, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+interface ListType {
+  id: number;
+  title: string;
+  body: string;
+}
 function App() {
+  const [post, setPost] = useState<null | ListType[]>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        const result = await fetch(url).then((data) => data.json());
+        setPost(result);
+        console.log(post);
+        return result;
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Open App.tsx and start build your app!!</Text>
+    <SafeAreaView>
+      <View>
+        {post?.length !== 0 ? (
+          <FlatList
+            data={post}
+            renderItem={({ item }) => (
+              <ScrollView
+                key={item.id}
+                style={{ padding: 20, backgroundColor: "#eee", margin: 10 }}
+              >
+                <Text>{item.id}</Text>
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  {item.title}
+                </Text>
+                <Text>{item.body}</Text>
+              </ScrollView>
+            )}
+          />
+        ) : null}
       </View>
-    </>
+    </SafeAreaView>
   );
 }
 
