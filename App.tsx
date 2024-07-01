dcd9947fadeac3416919baef257e258f7882b1f4
Zlaam <mrzalaam@gmail.com>
2024-07-01 17:04:17 +0500

    "Refactor App.tsx to use bottom tab navigator, remove HomeScreen and LoginScreen, and add new Login and SignUp components"

--- App.tsx
+++ App.tsx
@@ -1,44 +1,34 @@
+import {} from "react";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import HomeScreen from "./screens/Home";
-import LoginScreen from "./screens/LoginScreen";
-
-type RootStackParamList = {
-  Login: undefined;
-  Home: {
-    name: string;
-    age: number;
-  };
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: "skyblue",
-          },
-
-          headerTitleStyle: { fontSize: 24 },
-          animation: "fade_from_bottom",
-          statusBarColor: "black",
-          statusBarStyle: "inverted",
-        }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="SignUp" component={SignUp} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 export default App;
+
+export function Login() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 40 }}>Login</Text>
+    </View>
+  );
+}
+export function SignUp() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 40 }}>SignUp</Text>
+    </View>
+  );
+}
