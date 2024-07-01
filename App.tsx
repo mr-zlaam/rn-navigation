b9f81b317a01148eaefb32b82b8d4a30c583acd3
Zlaam <mrzalaam@gmail.com>
2024-07-01 17:14:37 +0500

    "Remove navigation and screens, replace with basic 'Hello World' app"

--- App.tsx
+++ App.tsx
@@ -1,43 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import HomeScreen from "./screens/Home";
-import LoginScreen from "./screens/LoginScreen";
-
-type RootStackParamList = {
-  Login: undefined;
-  Home: {
-    name: string;
-    age: number;
-  };
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import {} from "react";
+import { View, Text } from "react-native";
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: "skyblue",
-          },
-
-          headerTitleStyle: { fontSize: 24 },
-          animation: "fade_from_bottom",
-          statusBarColor: "black",
-          statusBarStyle: "inverted",
-        }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Open App.tsx and start build your app!!</Text>
+      </View>
+    </>
   );
 }
 
